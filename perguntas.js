969ddaac3f43b59ecf3cd1095a46a29706f4c9d2
julyrt24 <0000111219163xsp@aluno.educacao.sp.gov.br>
2024-11-04 17:56:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartac(
+    'Programação',
+    'O que é Python?',
+    'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    'O Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês)',
+    'Oi em inglês é HI (RAI)'
+)
